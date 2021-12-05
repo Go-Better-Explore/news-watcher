0236317494911f0b30acc3fdc19e181256472687
Go Better <78648274+Go-Better-Explore@users.noreply.github.com>
2021-12-05 16:39:58 +0300

    Add main module

---
+++ news-watcher-main/src/main/java/ru/gobetter/newswatcher/main/Application.java
@@ -0,0 +1,12 @@
+package ru.gobetter.newswatcher.main;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        run(Application.class, args);
+    }
+}

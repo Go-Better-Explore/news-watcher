e682d8fbe33d375c25455e637583e53889871557
Go Better <78648274+Go-Better-Explore@users.noreply.github.com>
2021-12-19 18:16:43 +0300

    Add testcontainers selenium execution and some improvements to vesti extractor

--- news-reader-selenium-integration/src/main/java/ru/gobetter/newswatcher/web/interaction/selenium/SeleniumBeans.java
+++ news-reader-selenium-integration/src/main/java/ru/gobetter/newswatcher/web/interaction/selenium/SeleniumBeans.java
@@ -1,48 +1,35 @@
 package ru.gobetter.newswatcher.web.interaction.selenium;
 
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 
-import java.io.File;
-import java.nio.file.Paths;
-import java.util.List;
+import static java.util.Objects.requireNonNull;
 
-import static java.lang.System.setProperty;
-
+@Slf4j
 @Configuration
 class SeleniumBeans {
-    private static final String DRIVER_EXECUTABLE_PROPERTY_KEY = "webdriver.gecko.driver";
-    private static final String UBLOCK_FILE_NAME = "uBlock.xpi";
-    private static final String DECENTRALEYES_FILE_NAME = "decentraleyes.xpi";
-    @Value("${" + DRIVER_EXECUTABLE_PROPERTY_KEY + "}")
-    private String pathToExecutable;
-
     @Bean
     WebDriver getSeleniumDriver() {
-        setProperty(DRIVER_EXECUTABLE_PROPERTY_KEY, pathToExecutable);
         val profile = new FirefoxProfile();
         profile.setPreference("permissions.default.image", 2);
         profile.setPreference("network.dns.disablePrefetchFromHTTPS", false);
         profile.setPreference("network.predictor.enable-prefetch", false);
-
-        val addonsDirectory = new File(pathToExecutable).getParentFile().getAbsolutePath()
-            + File.separator + "addons";
-        val addons = List.of(
-            UBLOCK_FILE_NAME,
-            DECENTRALEYES_FILE_NAME
-        );
         val options = new FirefoxOptions();
         options.setProfile(profile);
-        val driver = new FirefoxDriver(options);
-        addons.stream()
-            .map(fileName -> Paths.get(addonsDirectory, fileName))
-            .forEach(driver::installExtension);
-        return driver;
+        val network = Network.newNetwork();
+        val container = new BrowserWebDriverContainer<>()
+            .withCapabilities(options)
+            .withNetwork(network)
+            .withLogConsumer(new Slf4jLogConsumer(log));
+        container.start();
+        return requireNonNull(container.getWebDriver());
     }
 }
